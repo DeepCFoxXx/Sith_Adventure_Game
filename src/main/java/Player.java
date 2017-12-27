04e6846a6b9d5b0fe191116def5e1b91fd0896fd
DeepCFoxXx <learncode23@gmail.com>
2017-12-27 14:41:25 +0000

    Player class started

---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+public abstract class Player {
+
+    private String name;
+    private int health;
+
+    public Player(String name, int health){
+        this.name = name;
+        this.health = health;
+    }
+
+}
