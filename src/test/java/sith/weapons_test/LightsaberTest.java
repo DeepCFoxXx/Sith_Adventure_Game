688874a2210ebcb34bbdb78052191e0eb0d7562c
DeepCFoxXx <learncode23@gmail.com>
2017-12-28 12:10:56 +0000

    10 Tests done refactoring still to do

---
+++ src/test/java/sith/weapons_test/LightsaberTest.java
@@ -0,0 +1,28 @@
+package sith.weapons_test;
+
+import enemies.Enemy;
+import enemies.Finn;
+import org.junit.Before;
+import org.junit.Test;
+import sith.weapons.Lightsaber;
+
+import static org.junit.Assert.assertEquals;
+
+public class LightsaberTest {
+
+    Lightsaber lightsaber;
+    Enemy enemy;
+
+    @Before
+    public void before(){
+        lightsaber = new Lightsaber();
+        enemy = new Finn(40, 250);
+    }
+
+    @Test
+    public void canDamage(){
+        lightsaber.attack(enemy);
+        assertEquals(100, enemy.getHealth());
+    }
+
+}
