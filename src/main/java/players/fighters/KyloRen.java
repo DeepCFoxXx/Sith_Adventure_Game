9d7cf5e1edf526eb431234445c2c9d89e35c3251
DeepCFoxXx <learncode23@gmail.com>
2017-12-28 16:32:51 +0000

    Last player class tested and passed

--- src/main/java/players/fighters/KyloRen.java
+++ src/main/java/players/fighters/KyloRen.java
@@ -2,13 +2,28 @@ package players.fighters;
 
 import sith.weapons.IWeapon;
 
+import java.util.Random;
+
 public class KyloRen extends Fighter {
 
     private int rage;
 
     public KyloRen(String name, int health, IWeapon weapon) {
         super(name, health, weapon);
-        this.rage = rage;
+        this.rage = 0;
     }
 
+    public void canStartToRage() {
+        Random rand = new Random();
+        int amountOfRage = rand.nextInt(10);
+        this.rage += amountOfRage;
+    }
+
+    public int getRage() {
+        return rage;
+    }
+
+    public String speak() {
+        return " We're not done yet.";
+    }
 }
---
+++ src/test/java/fighters_test/KyloRenTest.java
@@ -0,0 +1,68 @@
+package fighters_test;
+
+import enemies.Enemy;
+import enemies.Rey;
+import org.junit.Before;
+import org.junit.Test;
+import players.fighters.KyloRen;
+import sith.weapons.CrossGuardLightsaber;
+import sith.weapons.IWeapon;
+import sith.weapons.Lightsaber;
+
+import static org.junit.Assert.assertEquals;
+
+public class KyloRenTest {
+
+    KyloRen kyloRen;
+    Enemy enemy;
+    IWeapon crossGuardLightsaber;
+    IWeapon lightsaber;
+
+    @Before
+    public void before() {
+        crossGuardLightsaber = new CrossGuardLightsaber();
+        lightsaber = new Lightsaber();
+        kyloRen = new KyloRen("Kylo Ren", 600, crossGuardLightsaber);
+        enemy = new Rey(120, 600, "Rey");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Kylo Ren", kyloRen.getName());
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(600, kyloRen.getHealth());
+    }
+
+    @Test
+    public void canRage() {
+        kyloRen.canStartToRage();
+        assertEquals(true, kyloRen.getRage() > 0);
+    }
+
+    @Test
+    public void canSpeak() {
+        assertEquals(" We're not done yet.", kyloRen.speak());
+    }
+
+    @Test
+    public void canSwapWeapons() {
+        kyloRen.setWeapon(lightsaber);
+        kyloRen.attack(enemy);
+        assertEquals(450, enemy.getHealth());
+    }
+
+    @Test
+    public void canTakeDamage() {
+        kyloRen.takeDamage(120);
+        assertEquals(480, kyloRen.getHealth());
+    }
+
+    @Test
+    public void canAttack() {
+        kyloRen.attack(enemy);
+        assertEquals(420, enemy.getHealth());
+    }
+}
