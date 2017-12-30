37e03bd3240032865ebd8261f54ca56ffb5b7303
DeepCFoxXx <learncode23@gmail.com>
2017-12-30 15:51:15 +0000

    1st user test passed

--- src/test/java/force_users_test/DarthSidiousTest.java
+++ src/test/java/force_users_test/DarthSidiousTest.java
@@ -1,4 +1,39 @@
 package force_users_test;
 
+import enemies.Enemy;
+import enemies.MaceWindu;
+import force_users.force_power_users.DarthSidious;
+import force_users.force_powers.ForceLightning;
+import force_users.force_powers.ForcePush;
+import org.junit.Before;
+import org.junit.Test;
+import summons.DeathStar;
+import summons.SuperStarDestroyer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DarthSidiousTest {
+
+    DarthSidious darthSidious;
+    Enemy enemy;
+    ForceLightning forceLightning;
+    ForcePush forcePush;
+    DeathStar deathStar;
+    SuperStarDestroyer superStarDestroyer;
+
+    @Before
+    public void before() {
+        darthSidious = new DarthSidious("Darth Sidious", 400, forceLightning, deathStar);
+        enemy = new MaceWindu(100, 500, "Mace Windu");
+        forceLightning = new ForceLightning();
+        forcePush = new ForcePush();
+        deathStar = new DeathStar();
+        superStarDestroyer = new SuperStarDestroyer();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Darth Sidious", darthSidious.getName());
+    }
+    
 }
