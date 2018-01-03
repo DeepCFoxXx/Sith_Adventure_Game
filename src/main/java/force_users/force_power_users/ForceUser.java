aa7b6257a208c05620c0bfcf7c47966d8c04e4ee
DeepCFoxXx <learncode23@gmail.com>
2018-01-03 15:00:47 +0000

    Use + Summon method passing

--- src/main/java/force_users/force_power_users/ForceUser.java
+++ src/main/java/force_users/force_power_users/ForceUser.java
@@ -5,7 +5,8 @@ import force_users.force_powers.IAbility;
 import players.Player;
 import summons.ISummons;
 
-public class ForceUser extends Player {
+
+public abstract class ForceUser extends Player {
 
     private IAbility ability;
     private ISummons summons;
@@ -16,20 +17,23 @@ public class ForceUser extends Player {
         this.summons =summons;
     }
 
-    public void use(Enemy enemy) {
-        this.ability.use(enemy);
-    }
-
     public void setAbility(IAbility ability) {
         this.ability = ability;
     }
 
-    public void summon(Enemy enemy) {
-        this.summons.summon(enemy);
-    }
+//    public void summon(Enemy enemy) {
+//        this.summons.summon(enemy);
+//    }
 
     public void setSummons(ISummons summons) {
         this.summons = summons;
     }
 
+    public void use(Enemy enemy) {
+        this.ability.use(enemy);
+    }
+
+    public void summon(Enemy enemy) {
+        this.summons.summon(enemy);
+    }
 }
--- src/test/java/force_users_test/DarthSidiousTest.java
+++ src/test/java/force_users_test/DarthSidiousTest.java
@@ -6,8 +6,11 @@ import force_users.force_power_users.DarthSidious;
 import force_users.force_powers.ForceLightning;
 import force_users.force_powers.ForcePush;
 import org.junit.Before;
+import org.junit.Test;
 import summons.DeathStar;
 import summons.SuperStarDestroyer;
+
+import static org.junit.Assert.assertEquals;
 
 public class DarthSidiousTest  {
 
@@ -26,6 +29,24 @@ public class DarthSidiousTest  {
         forcePush = new ForcePush();
         deathStar = new DeathStar();
         superStarDestroyer = new SuperStarDestroyer();
+    }
+
+    @Test
+    public void canUseAbility() {
+        darthSidious = new DarthSidious("Darth Sidious", 600, forceLightning, deathStar);
+        enemy = new MaceWindu(150, 500, "Mace Windu");
+        forceLightning = new ForceLightning();
+        darthSidious.use(enemy);
+        assertEquals(400, enemy.getHealth());
+    }
+
+    @Test
+    public void canUseSummon() {
+        darthSidious = new DarthSidious("Darth Sidious", 600, forceLightning, deathStar);
+        enemy = new MaceWindu(150, 500, "Mace Windu");
+        deathStar = new DeathStar();
+        darthSidious.summon(enemy);
+        assertEquals(-500, enemy.getHealth());
     }
 
 }
